31cdc4943280bb33b1cd699a9b70e7491a7803f5
FlyKite <DogeFlyKite@gmail.com>
2017-07-28 23:53:13 +0800

    Initial commit

---
+++ FKFakeTransition/FKFakeTransition.swift
@@ -0,0 +1,13 @@
+//
+//  FKFakeTransition.swift
+//  FKFakeTransition
+//
+//  Created by FlyKite on 2017/7/28.
+//  Copyright © 2017年 FlyKite. All rights reserved.
+//
+
+import UIKit
+
+class FKFakeTransition: NSObject {
+
+}
