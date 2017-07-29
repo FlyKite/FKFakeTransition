a05a9789d52d3b82ee62f6dd80daf25f14e0a826
FlyKite <DogeFlyKite@gmail.com>
2017-07-29 09:50:00 +0800

    Prepare to transition

--- FKFakeTransition/FKFakeTransition.swift
+++ FKFakeTransition/FKFakeTransition.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 public protocol FKFakeTransitionCreator {
+    
+    /// Create FKFakeTransitioning with key window.
+    ///
+    /// - parameter keyWindow: The key window of Application
+    /// - returns: The transition
     static func create(with keyWindow: UIWindow) -> FKFakeTransitioning
 }
 
 public protocol FKFakeTransitioning {
     
+    /// This function should call before push a ViewController into NavigationController to add a screenshot on key window.
+    func prepareFakeTransitioning()
+    
+    /// This function should call in viewDidLoad of the controller pushed into NavigationController, then transition will be performed.
+    func performFakeTransitioning()
 }
 
-class FKFakeTransition {
+public class FKFakeTransition {
     var window: UIWindow
     init(_ keyWindow: UIWindow) {
         window = keyWindow
+    }
+    
+    func shotWindow(_ rect: CGRect) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(rect.size, true, UIScreen.main.scale)
+        let frame = CGRect(origin: CGPoint(x: -rect.origin.x, y: -rect.origin.y), size: self.window.bounds.size)
+        self.window.drawHierarchy(in: frame, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
 }
--- FKFakeTransition/FKPaperFoldingTransition.swift
+++ FKFakeTransition/FKPaperFoldingTransition.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-public class FKPaperFoldingTransition: FKFakeTransitionCreator {
+public class FKPaperFoldingTransition: NSObject, FKFakeTransitionCreator {
+
     public static func create(with keyWindow: UIWindow) -> FKFakeTransitioning {
         let transition = FKPaperFoldingTransitioning(keyWindow)
         return transition
     }
 }
 
-class FKPaperFoldingTransitioning: FKFakeTransition, FKFakeTransitioning {
-
+public class FKPaperFoldingTransitioning: FKFakeTransition, FKFakeTransitioning {
+    
+    public var navigationBarHeight: CGFloat = 64
+    public var foldingFromY: CGFloat = UIScreen.main.bounds.size.height / 2
+    
+    private var prepared = false
+    
+    private var animationView: UIView!
+    private var navigationBarImageView: UIImageView!
+    private var viewTopImageView: UIImageView!
+    private var viewBottomImageView: UIImageView!
+    
+    public func prepareFakeTransitioning() {
+        let windowWidth = self.window.bounds.width
+        let windowHeight = self.window.bounds.height
+        
+        animationView = UIView(frame: window.bounds)
+        animationView.backgroundColor = UIColor.black
+        
+        let viewTopImage = self.shotWindow(CGRect(x: 0, y: self.navigationBarHeight, width: windowWidth, height: self.foldingFromY - self.navigationBarHeight))
+        viewTopImageView = UIImageView(image: viewTopImage)
+        viewTopImageView.frame = CGRect(x: 0, y: self.navigationBarHeight, width: windowWidth, height: self.foldingFromY - self.navigationBarHeight)
+        animationView.addSubview(viewTopImageView)
+        
+        let viewBottomImage = self.shotWindow(CGRect(x: 0, y: self.foldingFromY, width: windowWidth, height: windowHeight - self.foldingFromY))
+        viewBottomImageView = UIImageView(image: viewBottomImage)
+        viewBottomImageView.frame = CGRect(x: 0, y: self.foldingFromY, width: windowWidth, height: windowHeight - self.foldingFromY)
+        animationView.addSubview(viewBottomImageView)
+        
+        let navigationBarImage = self.shotWindow(CGRect(x: 0, y: 0, width: windowWidth, height: self.navigationBarHeight))
+        navigationBarImageView = UIImageView(image: navigationBarImage)
+        navigationBarImageView.frame = CGRect(x: 0, y: 0, width: windowWidth, height: self.navigationBarHeight)
+        animationView.addSubview(navigationBarImageView)
+        
+        self.window.addSubview(animationView)
+        
+        self.prepared = true
+    }
+    
+    public func performFakeTransitioning() {
+        if !self.prepared {
+            print("ERROR: prepareFakeTransitioning should call before preformFakeTransitioning!")
+            return
+        }
+        
+        
+    }
 }
